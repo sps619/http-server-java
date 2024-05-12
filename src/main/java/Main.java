12a843e497c2fcbc034552e4295ef605dcf11cbf
sps619 <shubham-thakur@live.com>
2024-05-13 02:57:20 +0530

    Stage 4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -17,18 +17,37 @@ public class Main {
       serverSocket = new ServerSocket(4221);
       serverSocket.setReuseAddress(true);
       clientSocket = serverSocket.accept(); // Wait for connection from client.
-      String httpOKResponse = "HTTP/1.1 200 OK\r\n\r\n";
-      String http404Response = "HTTP/1.1 404 Not Found\r\n\r\n";
+      String httpOKResponse = "HTTP/1.1 200 OK";
+      String http404Response = "HTTP/1.1 404 Not Found";
+      String crlf = "\r\n";
+      String contentType = "Content-Type: text/plain";
+      String contentLength = "Content-Length: ";
       OutputStream clientOutput = clientSocket.getOutputStream();
 
       BufferedReader bufferReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-      if(bufferReader.readLine().split(" ")[1].equals("/")){
-        clientOutput.write(httpOKResponse.getBytes(StandardCharsets.UTF_8));
-      }
-      else{
-        clientOutput.write(http404Response.getBytes(StandardCharsets.UTF_8));
+      String input = bufferReader.readLine();
+      if(input.split(" ")[1].equals("/")){
+        clientOutput.write(httpOKResponse.getBytes());
+        clientOutput.write(crlf.getBytes());
+        clientOutput.write(crlf.getBytes());
       }
 
+      else if(input.contains("/echo/")){
+        clientOutput.write(httpOKResponse.getBytes());
+        clientOutput.write(crlf.getBytes());
+        clientOutput.write(contentType.getBytes());
+        clientOutput.write(crlf.getBytes());
+        clientOutput.write(contentLength.getBytes());
+        clientOutput.write(String.valueOf(input.split(" ")[1].split("/echo/")[1].length()).getBytes());
+        clientOutput.write(crlf.getBytes());
+        clientOutput.write(crlf.getBytes());
+        clientOutput.write(input.split(" ")[1].split("/echo/")[1].getBytes());
+      }
+      else{
+        clientOutput.write(http404Response.getBytes());
+        clientOutput.write(crlf.getBytes());
+        clientOutput.write(crlf.getBytes());
+      }
       clientOutput.flush();
       clientOutput.close();
     } catch (IOException e) {
