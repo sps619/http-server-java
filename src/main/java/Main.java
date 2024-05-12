08356c0a20d06f5970972af9f59807049bf66c15
sps619 <shubham-thakur@live.com>
2024-05-13 01:13:45 +0530

    Third Stage 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -22,7 +22,7 @@ public class Main {
       OutputStream clientOutput = clientSocket.getOutputStream();
 
       BufferedReader bufferReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-      if(bufferReader.readLine().contain("/")){
+      if(bufferReader.readLine().contains("/")){
         clientOutput.write(httpOKResponse.getBytes(StandardCharsets.UTF_8));
       }
       else{
